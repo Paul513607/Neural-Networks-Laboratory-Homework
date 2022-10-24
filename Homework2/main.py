5ddffa710c79bded0cecc382fb9e5f51c15bc5b9
Paul513607 <paul.samson42@gmail.com>
2022-10-24 18:25:18 +0300

    matrix multiplication method

--- Homework2/main.py
+++ Homework2/main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     perceptron_list = []
     for i in range(0, 10):
         perceptron = Perceptron(i)
-        perceptron.mini_batch(train_set[0], train_set[1])
+        perceptron.mini_batch2(train_set[0], train_set[1])
         print("[Train] Accuracy for ", i, ": ", perceptron.accuracy(train_set[0], train_set[1]))
         perceptron_list.append(perceptron)
 
@@ -19,7 +19,3 @@ if __name__ == '__main__':
 
     for i in range(0, 10):
         print("[Test] Accuracy for ", i, ": ", perceptron_list[i].accuracy(test_set[0], test_set[1]))
-
-
-
-
--- Homework2/perceptron.py
+++ Homework2/perceptron.py
@@ -3,16 +3,20 @@ from sklearn.utils import shuffle
 
 
 def activation(z):
-    return 1 if z >= 0 else 0
+    return z > 0
 
 
-def shuffle(train_set, train_set_labels):
+def activation2(z_array):
+    return np.array(list(map(lambda z: z > 0, z_array)))
+
+
+def shuffle_set(train_set, train_set_labels):
     tmp_set1, tmp_set2 = shuffle(train_set, train_set_labels, random_state=0)
     return tmp_set1, tmp_set2
 
 
 class Perceptron:
-    def __init__(self, speciality_value, lr=0.01, epochs=15):
+    def __init__(self, speciality_value, lr=0.01, epochs=10):
         self.W = None
         self.B = None
 
@@ -24,14 +28,18 @@ class Perceptron:
         train_set, train_set_labels = shuffle(train_set, train_set_labels)
         train_set_labels = np.where(train_set_labels == self.speciality_value, 1, 0)
 
-        number_of_batches = len(train_set[0]) // batch_size
-        shuffle()
+        # Calculate the number of batches
+        number_of_batches = len(train_set) // batch_size
+        # Shuffle the data
+        train_set, train_set_labels = shuffle_set(train_set, train_set_labels)
 
+        # Initialize the weights and bias
         self.W = np.random.rand(len(train_set[0]) + 1)
         self.B = np.random.rand(1)
         self.W[0] = self.B
 
         for i in range(0, self.epochs):
+            # Iterate over batches
             for j in range(0, number_of_batches):
                 batch = train_set[j * batch_size:(j + 1) * batch_size]
                 batch_labels = train_set_labels[j * batch_size:(j + 1) * batch_size]
@@ -40,6 +48,7 @@ class Perceptron:
                 bias_delta = 0
                 weights_delta[0] = bias_delta
 
+                # Iterate over samples in batch and calculate deltas
                 for x, t in zip(batch, batch_labels):
                     x = np.insert(x, 0, 1)
                     z = np.dot(x, self.W) + self.B
@@ -47,11 +56,48 @@ class Perceptron:
                     weights_delta += (t - y) * x * self.lr
                     bias_delta += (t - y) * self.lr
 
+                # Update weights and bias
                 self.W += weights_delta
                 self.B += bias_delta
-
             print(f"[{self.speciality_value}] Epoch: {i + 1} Accuracy: {self.accuracy(train_set, train_set_labels, True)}")
 
+    def mini_batch2(self, train_set, train_set_labels, batch_size=32):
+        train_set, train_set_labels = shuffle(train_set, train_set_labels)
+        train_set_labels = np.where(train_set_labels == self.speciality_value, 1, 0)
+
+        # Calculate the number of batches
+        number_of_batches = len(train_set) // batch_size
+        # Shuffle the data
+        train_set, train_set_labels = shuffle_set(train_set, train_set_labels)
+
+        # Initialize the weights and bias
+        self.W = np.random.rand(len(train_set[0]) + 1)
+        self.B = np.random.rand(1)
+        self.W[0] = self.B
+
+        for i in range(0, self.epochs):
+            # Iterate over batches
+            for j in range(0, number_of_batches):
+                batch = train_set[j * batch_size:(j + 1) * batch_size]
+                batch = np.array([np.insert(x, 0, 1) for x in batch])
+                batch_labels = np.array(train_set_labels[j * batch_size:(j + 1) * batch_size])
+
+                # Calculate the weights and bias deltas using matrix operations
+                z = np.dot(batch, self.W) + self.B
+                y = activation2(z)
+                error = np.array(batch_labels - y)[np.newaxis]
+                product = np.dot(error, batch).T * self.lr
+                product = product.reshape(len(product))
+
+                weights_delta = product * self.lr
+                bias_delta = np.sum(error * self.lr)
+
+                # Update weights and bias
+                self.W += weights_delta
+                self.B += bias_delta
+            print(f"[{self.speciality_value}] Epoch: {i + 1} Accuracy: {self.accuracy(train_set, train_set_labels, True)}")
+
+    # Calculate the accuracy of the perceptron for the current sets of values and labels
     def accuracy(self, test_set, test_set_labels, running=False):
         if not running:
             test_set_labels = np.where(test_set_labels == self.speciality_value, 1, 0)
